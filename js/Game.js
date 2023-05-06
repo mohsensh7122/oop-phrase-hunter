ff633771c0016b0e0a2f3323f2964c4e3c7e7fcd
Mohsen Shirazi <mohsensh4792@gmail.com>
2023-05-06 15:44:14 -0400

    Added the removeLife method to the Game class.
    This method removes a life from the scoreboard, by replacing
    one of the `liveHeart.png` images with a `lostHeart.png` image (found in the
    `images` folder) and increments the `missed` property. If the player has five missed guesses (i.e they're out of lives), then the game will end by calling the `gameOver()` method.

--- js/Game.js
+++ js/Game.js
@@ -28,18 +28,39 @@ class Game {
     }
 
     /**
-* Checks for winning move
-* @return {boolean} True if game has been won, false if game wasn't
-won
-*/
+    * Checks for winning move
+    * @return {boolean} True if game has been won, false if game wasn't
+    won
+    */
+
     checkForWin() {
         const hiddenLetters = document.querySelectorAll('.hide');
 
-        if(hiddenLetters > 0){
+        if (hiddenLetters > 0) {
             return false;
         } else {
             return true;
         }
     };
+
+    /**
+     * Increases the value of the missed property
+     * Removes a life from the scoreboard
+     * Checks if player has remaining lives and ends game if player is out
+     */
+
+    removeLife() {
+
+        const hearts = document.querySelectorAll('.tries img');
+        this.missed++;
+        console.log(this.missed);
+        hearts[this.missed - 1].src = 'images/lostHeart.png';
+
+        if (this.missed === 5){
+            this.gameOver()
+        }
+    };
+
+    
 
 }
